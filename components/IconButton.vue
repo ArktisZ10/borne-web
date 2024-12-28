c52f8ed661973c2b26552d02a0642f016a904b2d
Erik Börne <erik.borne@outlook.com>
2024-12-28 01:00:14 +0100

    Add IconButton component
    Clickable icon link

---
+++ components/IconButton.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { to: destination } = defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <NuxtLink :to="destination" target="_blank">
+    <div :class="$attrs.class" class="p-2 font-semibold rounded-lg" />
+  </NuxtLink>
+</template>
